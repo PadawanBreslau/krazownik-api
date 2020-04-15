a57fc8500bbba47b8228cc782f8adfc9cb43b544
Staszek <s.zawadzki@visuality.pl>
2020-04-15 10:13:11 +0200

    Remove unneccesary authentication

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class EventsController < Api::BaseController
-      before_action :authenticate_api_v1_user!
-
       def index
         year = SelectProperYearLogic.year
         event = Event.find_by(year: year)
