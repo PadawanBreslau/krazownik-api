5697306684f65fafc21b2abfa7b3f7e8bb25f44b
Staszek <s.zawadzki@visuality.pl>
2020-04-20 09:10:39 +0200

    Production env api host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Rails.application.routes.default_url_options[:host] = ENV['BACKEND_APP_URL']
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
