76dc9937008ee275d4db4f596cb137c121659032
Staszek <s.zawadzki@visuality.pl>
2020-05-18 17:13:54 +0200

    Bonus Point order by points

--- app/models/bonus_point.rb
+++ app/models/bonus_point.rb
@@ -1,4 +1,5 @@
 class BonusPoint < ApplicationRecord
+  default_scope { order('points ASC') }
   belongs_to :event
   has_many :bonus_point_completions, dependent: :destroy
   has_many :participations, through: :bonus_point_completions
