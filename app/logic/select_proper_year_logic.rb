de98f64f2944b13e1a879f734615aecd89a5ee94
Staszek <s.zawadzki@visuality.pl>
2020-06-01 10:33:57 +0200

    Fix for proper year

--- app/logic/select_proper_year_logic.rb
+++ app/logic/select_proper_year_logic.rb
@@ -2,7 +2,7 @@ class SelectProperYearLogic
   def self.year
     current_date = Date.today
 
-    if current_date.month < 6
+    if current_date.month < 7
       current_date.year
     else
       current_date.year + 1
