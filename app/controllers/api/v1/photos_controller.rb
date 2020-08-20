54b506e4e8d8ac80a78edf3057b252b489a5ff71
Staszek <s.zawadzki@visuality.pl>
2020-08-20 23:50:57 +0200

    Dont return too big files

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class PhotosController < Api::BaseController
       def index
-        @photos = ActiveStorage::Attachment.where(name: 'photos').shuffle
+        @photos = ActiveStorage::Attachment.where(name: 'photos').select{|att| att.blob.byte_size < 1048576}.shuffle
 
         render json: PhotoSerializer.new(@photos).serialized_json
       end
