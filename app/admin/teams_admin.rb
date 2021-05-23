a1fc7b352f5ba6d17a5f43c3545823caadd3a6a6
Staszek <s.zawadzki@visuality.pl>
2021-05-23 08:11:31 +0200

    Team panel changes

--- app/admin/teams_admin.rb
+++ app/admin/teams_admin.rb
@@ -13,21 +13,33 @@ Trestle.resource(:teams) do
   end
 
   form do |team|
-    row do
-      col { text_field :name }
-      col { select :leader_participation_id, team.participations.map { |t| [t.name, t.id] }, label: 'Leader' } if team.persisted?
-      col { select :event_id, Event.all.map { |e| [e.year, e.id] }, label: 'Event' }
-    end
-
-    row do
-      active_storage_field :emblem
-    end
-
-    divider
-
-    team.participations.map do |p|
+    tab :basic do
       row do
-        p.name
+        col { text_field :name }
+        col { select :leader_participation_id, team.participations.map { |t| [t.name, t.id] }, label: 'Leader' } if team.persisted?
+        col { select :event_id, Event.all.map { |e| [e.year, e.id] }, label: 'Event' }
+      end
+
+      row do
+        active_storage_field :emblem
+      end
+    end
+
+    tab :participations, badge: team.participations.size do
+      table team.participations, admin: :participations do
+        column :name, link: true
+
+        actions
+      end
+    end
+
+    tab :tasks, badge: team.team_tasks.size do
+      table team.team_tasks, admin: :team_tasks do
+        column :content, link: true
+        column :amount
+        column :completed_at
+
+        actions
       end
     end
   end
