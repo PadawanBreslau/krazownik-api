76b120ee52f9b045069d62a484d18a34803f5d39
Staszek <s.zawadzki@visuality.pl>
2021-05-23 20:21:57 +0200

    Better participation view

--- app/admin/participations_admin.rb
+++ app/admin/participations_admin.rb
@@ -7,4 +7,30 @@ Trestle.resource(:participations) do
       item :participations, icon: 'fa fa-star'
     end
   end
+
+  table(autolink: false) do
+    column :event do |object|
+      object.event.year
+    end
+
+    column :name do |object|
+      object.user.name
+    end
+
+    column :email do |object|
+      object.user.email
+    end
+
+    column :team do |object|
+      object.team&.name
+    end
+
+    column :team_ready do |object|
+      object.user.team_ready
+    end
+
+    column :phone do |object|
+      object.user.phone_number
+    end
+  end
 end
